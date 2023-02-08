d639ebb46488317214540ffb7bce86f7c54ba01c
TACC Staff <staff@wireless-10-155-51-8.public.utexas.edu>
2023-02-08 14:48:08 -0600

    added parallelism

--- assets/runner.sh
+++ assets/runner.sh
@@ -35,4 +35,4 @@ then
      flex='Empty'
 fi
     
-MV2_ENABLE_AFFINITY=0 ibrun -np 32 singularity exec vina_1.2.3.sif python3 autodock.py -r ${receptor} -c "${center_x},${center_y},${center_z}" -s "${size_x},${size_y},${size_z}" -m ${forcefield} -d ${docking} -ll ${library} -n ${top_n_scores} -f $flex
+MV2_ENABLE_AFFINITY=0 ibrun singularity exec vina_1.2.3.sif python3 autodock.py -r ${receptor} -c "${center_x},${center_y},${center_z}" -s "${size_x},${size_y},${size_z}" -m ${forcefield} -d ${docking} -ll ${library} -n ${top_n_scores} -f $flex
