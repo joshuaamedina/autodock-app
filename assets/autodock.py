1045b2f8c1bde14f051bb5388c5d3c7f38096874
TACC Staff <staff@wireless-10-155-51-8.public.utexas.edu>
2023-02-08 08:17:02 -0600

    updated autodock.py

--- assets/autodock.py
+++ assets/autodock.py
@@ -33,7 +33,7 @@ parser.add_argument('-s','--size',type=str,required=True,
 parser.add_argument('-m','--module',type=str,required=True,
                     help='module for docking')
 parser.add_argument('-d','--docking',type=str,required=True,
-                    help='rigid or flexible docking')
+                    help='basic or flexible docking')
 parser.add_argument('-f','--sidechains',type=str,required=False,
                     help='sidechains if flexible docking')
 parser.add_argument('-n','--number',type=int,required=True,
@@ -55,13 +55,11 @@ size_z = float(box[2])
 full_receptor=args.receptor
 receptor=full_receptor.split('.')[0]
 flex_receptor=f'{receptor}_flex'
-if docking == 'rigid':
+if docking == 'basic':
     flexible = False
 elif docking == 'flexible':
     flexible = True
     sidechains = (args.sidechains).split('_')
-
-subprocess.run([f'echo {sidechains} >> error.txt'],shell=True)
 docking_type = args.module
 ligand_library = args.ligand_library
 config_path = './configs/config.config'
@@ -72,33 +70,60 @@ user_configs = {'center_x': center_x,
                 'size_y': size_y,
                 'size_z': size_z}
 
-number_of_outputs = args.number
+
+number_of_outputs = args.number if args.number <= 1000 else 1000
 
 # Internal variables
 cpus = 4
 verbosity = 0
 poses = 1
+exhaustiveness = 8
 
 def check_user_configs():
-    assert (size_x <= 30 and size_y <= 30 and size_z <=30), \
-           "box size is outside the bounds (30)" 
-    assert (full_receptor.endswith('.pdb') or full_receptor.endswith('.pdbqt')), \
-           "Please provide a .pdb or .pdbqt file"
-
-def check_user_sidechains():
     # Check that user-inputted flexible sidechains are found within the .pdbqt file
-    if flexible == False:
-        return
+    for size in [size_x, size_y, size_z]:
+        if not (size <= 30 and size >= 1):
+           subprocess.run(["echo 'box size is outside the bounds (1-30)' \
+                           >> error.txt"], shell=True)
+           comm.Abort()
+    if not (full_receptor.endswith('.pdb') or full_receptor.endswith('.pdbqt')):
+        subprocess.run(["echo 'Please provide a .pdb or .pdbqt file' \
+                        >> error.txt"], shell=True)
+        comm.Abort()
+          
     all_sidechains = []
+    xbounds = []
+    ybounds = []
+    zbounds = []
     with open(f'{receptor}.pdbqt', 'r') as r:
         line = r.readline()
         while line:
             line = r.readline()
             if line.startswith('ATOM'):
-                 all_sidechains.append(line.split()[3] + line.split()[5])
-    for sidechain in sidechains:
-        assert sidechain in all_sidechains, \
-               "Please provide valid flexible sidechain names (e.g. THR315)"
+                xbounds.append(float(line.split()[6]))
+                ybounds.append(float(line.split()[7]))
+                zbounds.append(float(line.split()[8]))
+                all_sidechains.append(line.split()[3] + line.split()[5])
+    if flexible == True:
+        for sidechain in sidechains:
+            if not sidechain in all_sidechains:
+                subprocess.run(["echo 'Please provide valid flexible sidechain \
+                                names, separated by spaces (e.g. THR315 GLU268)' \
+                                >> error.txt"], shell=True)
+                comm.Abort()
+                  
+    if not (min(xbounds)) <= center_x <= (max(xbounds)):
+        subprocess.run(["echo 'Center x coordinate is not within bounds' \
+                        >> error.txt"], shell=True)
+        comm.Abort()
+    if not (min(ybounds)) <= center_y <= (max(ybounds)):
+        subprocess.run(["echo 'Center y coordinate is not within bounds' \
+                        >> error.txt"], shell=True)
+        comm.Abort()
+    if not (min(zbounds)) <= center_z <= (max(zbounds)):
+        subprocess.run(["echo 'Center z coordinate is not within bounds' \
+                        >> error.txt"], shell=True)
+        comm.Abort()
 
 
 def prep_config():
@@ -125,14 +150,18 @@ def prep_receptor():
     # Converts a PDB receptor to a PDBQT, if needed. If the user has specified 
     #   flexible docking, also prepares the rigid receptor and user-chosen 
     #   flexible sidechains.
-    if exists(f'{receptor}.pdb'):
-        subprocess.run([f'prepare_receptor -r {receptor}.pdb \
-                        -o {receptor}.pdbqt'], shell=True)
-    if flexible == True:
-        subprocess.run([f"pythonsh ./scripts/prepare_flexreceptor.py \
-                        -g {receptor}.pdbqt -r {receptor}.pdbqt \
-                        -s {'_'.join(sidechains)}"], shell=True)
-
+    try:
+        if exists(f'{receptor}.pdb'):
+            subprocess.run([f'prepare_receptor -r {receptor}.pdb \
+                            -o {receptor}.pdbqt'], shell=True)
+        if flexible == True:
+            subprocess.run([f"pythonsh ./scripts/prepare_flexreceptor.py \
+                            -g {receptor}.pdbqt -r {receptor}.pdbqt \
+                            -s {'_'.join(sidechains)}"], shell=True)
+    except:
+        subprocess.run([f"echo 'error on rank {rank}: error prepping receptor' \
+                        >> errors.txt"],shell=True)
+        comm.Abort()
 
 def prep_ligands():
     # Returns a list where each item is the path to a pickled and compressed 
@@ -154,7 +183,7 @@ def run_docking(ligands, v, directory):
     for index, filename in enumerate(ligands):
         ligand = ligands[filename]
         v.set_ligand_from_string(ligand)
-        v.dock()
+        v.dock(exhaustiveness=exhaustiveness)
         v.write_poses(f'{output_directory}/output_{filename}', \
                       n_poses=poses, overwrite=True)
         subprocess.run([f"grep -i -m 1 'REMARK VINA RESULT:' \
@@ -176,12 +205,11 @@ def unpickle_and_decompress(path_to_file):
 
 def pre_processing():
     # Helper function to reduce clutter in main()
-    subprocess.run(['mkdir -p configs output/pdbqt output/top_results'], shell=True)
-    check_user_configs()
+    subprocess.run(['mkdir -p configs output/pdbqt output/results/ligands'], shell=True)
     prep_config()
     prep_receptor()
     prep_maps()
-    check_user_sidechains()
+    check_user_configs()
 
 
 def processing():
@@ -213,13 +241,27 @@ def processing():
     count = 1
     directory = 1
     while True:
-        comm.send(rank,dest=0) # Ask rank 0 for another set of ligands
-        ligand_set_path = comm.recv(source=0) # Wait for a response
-        if ligand_set_path == 'no more ligands':
-            comm.send('message received--proceed to post-processing',dest=0)
-            break
-        ligands = unpickle_and_decompress(ligand_set_path)
-        run_docking(ligands, v, directory)
+        try:
+            comm.send(rank,dest=0) # Ask rank 0 for another set of ligands
+            ligand_set_path = comm.recv(source=0) # Wait for a response
+            if ligand_set_path == 'no more ligands':
+                comm.send('message received--proceed to post-processing',dest=0)
+                break
+        except:
+            subprocess.run([f"echo 'error on rank {rank}: communication error \
+                            with rank 0' >> errors.txt"],shell=True)
+        try:
+            ligands = unpickle_and_decompress(ligand_set_path)
+        except:
+            subprocess.run([f"echo 'error on rank {rank}: could not \
+                            unpickle/decompress {ligand_set_path}' \
+                            >> errors.txt"],shell=True)
+        try:
+            run_docking(ligands, v, directory)
+        except:
+            subprocess.run([f"echo 'error on rank {rank}: docking error with \
+                            ligand set {ligand_set_path}, ligands {ligands}' \
+                            >> errors.txt"],shell=True)
         count += 1
         if count == 100:
             count = 1
@@ -231,10 +273,10 @@ def sort():
     # Cats all results files into one, arranges each line to read: 
     #   (ligand, top score), then sorts by score so that highest scoring 
     #   ligands are on top; prints these sorted results are written to 
-    #   processed_results.txt; finally cleans up the directory
+    #   sorted_scores.txt; finally cleans up the directory
     subprocess.run(["cat results* >> results_merged.txt"], shell=True)
     INPUTFILE = 'results_merged.txt'
-    OUTPUTFILE = './output/top_results/processed_results.txt'
+    OUTPUTFILE = './output/results/sorted_scores.txt'
     
     result = []
 
@@ -247,17 +289,16 @@ def sort():
             line = data.readline()
 
     with open(OUTPUTFILE, 'w') as data:
-        data.writelines(sorted(result, key=lambda x: float(x.split()[1])))
+        data.writelines(sorted(result[:number_of_outputs], \
+                        key=lambda x: float(x.split()[1])))
     
 
 def isolate_output():
     # Copies the user-specified top n ligand output files to a single directory
     top_ligand_filenames = []
     
-    with open('./output/top_results/processed_results.txt', 'r') as results:
+    with open('./output/results/sorted_scores.txt', 'r') as results:
         for index, line in enumerate(results):
-            if index == (number_of_outputs): 
-                break
             top_ligand_filenames.append(line.split()[0])
 
     for dirpath, dirnames, filenames in os.walk('./output/pdbqt'):
@@ -265,8 +306,16 @@ def isolate_output():
             for filename in filenames:
                 if filename == f'output_{top_filename}':
                     subprocess.run([f'mv {dirpath}/{filename} \
-                                    ./output/top_results'], shell=True)
-    subprocess.run([f'tar -czf top_results.tar.gz ./output/top_results'],
+                                    ./output/results/ligands'], \
+                                    shell=True)
+    
+    with open('./output/results/combined_docked_ligands', 'w+') as combined:
+        for dirpath, dirnames, filenames in os.walk('./output/results/ligands'):
+            for filename in filenames:
+                lines = open(f'{dirpath}/{filename}', 'r').read()
+                combined.write(str(lines))
+    
+    subprocess.run([f'tar -czf results.tar.gz ./output/results'],
                    shell=True)
 
 
